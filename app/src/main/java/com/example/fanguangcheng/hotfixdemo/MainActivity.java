2adb2c3406b22fe4abd4018d4f5452a076da67ca
fanguangcheng <990152425@qq.com>
2018-01-26 17:37:39 +0800

    tinker 热修复demo，流程： 第一步，通过assembleDebug打包生成apk，安装 第二步，改动一些代码，比如加个toast提示，通过tinkerPatchDebug打包，会将第一步的apk和新的apk对比，生成patch_signed.apk，在build的apk的tinker目录下，放在手机的根目录后，打开app，点击按钮加载，patch成功会，和杀死进程闪退 然后再次打开，就第二步改的代码就会生效了

--- app/src/main/java/com/example/fanguangcheng/hotfixdemo/MainActivity.java
+++ app/src/main/java/com/example/fanguangcheng/hotfixdemo/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.fanguangcheng.hotfixdemo;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
     Button btn_click;
@@ -18,8 +24,24 @@ public class MainActivity extends AppCompatActivity {
         btn_click.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "btn click", Toast.LENGTH_LONG).show();
+                File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/patch_signed.apk");
+                Log.d("hotfix", "patch file is exist : " + file.exists());
+
+                Toast.makeText(MainActivity.this, "1 + 1 = ? " + 55555, Toast.LENGTH_LONG).show();
+
+                if (!file.exists()) {
+                    Log.d("hotfix", "file is not exist just return!");
+                    return;
+                }
+
+                //进行补丁的操作
+                TinkerInstaller.onReceiveUpgradePatch(MainActivity.this,
+                        Environment.getExternalStorageDirectory().getAbsolutePath()+"/patch_signed.apk");
             }
         });
+
+        File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/patch_signed.apk");
+        Log.d("hotfix", "patch file is exist : " + file.exists());
+
     }
 }
