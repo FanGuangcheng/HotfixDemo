1e1f94ef7a4a4a4bbc06472cb71769b0e7e16571
fanguanggcheng <fansu1990>
2018-01-30 19:02:30 +0800

    设置改动

--- hbpatch/src/main/java/com/husor/android/hbpatch/server/utils/Constants.java
+++ hbpatch/src/main/java/com/husor/android/hbpatch/server/utils/Constants.java
@@ -8,6 +8,6 @@ package com.husor.android.hbpatch.server.utils;
  * Created by fanguanggcheng on 2018/1/30.
  */
 public class Constants {
-    public static final String HOST_URL = "http://sapi.beibei.com";
+    public static final String HOST_URL = "http://163.netease.com";
 
 }
